db1e1f551ce1753fd0f3aae191433dfbe810adfb
hfsflj <147462589+hfsflj@users.noreply.github.com>
2023-11-09 19:42:18 +0900

    改善

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  
+  belongs_to :user
+  has_many :purchases
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :item_name, presence: true
+  validates :item_detail, presence: true
+  validates :item_price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :item_content, presence: true
+  validates :delivery_price, presence: true
+  validates :delivery_date, presence: true
+  validates :shipping area, presence: true
+  validates :category, presence: true
+
+end
