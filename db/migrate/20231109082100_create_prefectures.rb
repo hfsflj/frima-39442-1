73a021640bd118dd9dc3228f566b4c522ab3a58f
hfsflj <147462589+hfsflj@users.noreply.github.com>
2023-11-20 18:50:07 +0900

    改善

--- db/migrate/20231109082100_create_prefectures.rb
+++ db/migrate/20231109082100_create_prefectures.rb
@@ -2,6 +2,10 @@ class CreatePrefectures < ActiveHash::Base
   def change
     create_table :prefectures do |t|
 
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+
+
       t.timestamps
     end
   end
--- db/migrate/20231109102532_create_items.rb
+++ db/migrate/20231109102532_create_items.rb
@@ -1,6 +1,18 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :item_name, null: false
+      t.text :category, null: false
+      t.text :item_detail, null: false
+      t.integer :item_price, null: false
+      t.string :item_content
+      t.string :delivery_price
+      t.string :delivery_date
+      t.string :shipping_area
+      t.references :user, null: false, foreign_key: true
+
+      belongs_to :user
+      has_one :purchase
 
       t.timestamps
     end
