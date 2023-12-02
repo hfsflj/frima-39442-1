a0bb0b56c1c8c5817a11717b1dfb77664234de08
hfsflj <147462589+hfsflj@users.noreply.github.com>
2023-12-02 18:18:00 +0900

    改善

--- db/migrate/20231113104502_change_building_name_type_to_string_in_shipping_addresses.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBuildingNameTypeToStringInShippingAddresses < ActiveRecord::Migration[6.0]
-  def change
-    change_column :shipping_addresses, :building_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_11_01_065809) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,13 +24,14 @@ ActiveRecord::Schema.define(version: 0) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "users"
-  add_foreign_key "prefectures", "items"
-  add_foreign_key "prefectures", "users"
 end
