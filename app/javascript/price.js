3b21e64e1b005016e3c4faf237a3aa203ded0ca9
hfsflj <147462589+hfsflj@users.noreply.github.com>
2023-12-28 19:11:37 +0900

    修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   // 販売価格を入力する箇所の定義
-const inputPriceElement = document.getElementById('item-price');
-
+  const inputPriceElement = document.getElementById('item-price');
 
   // 販売手数料と販売利益の表示要素の定義
   const addTaxPriceElement = document.getElementById('add-tax-price');
   const profitElement = document.getElementById('profit');
 
   // 入力された価格が変更されたときのイベントリスナー
-  
   inputPriceElement.addEventListener('input', function() {
     const inputPrice = inputPriceElement.value;
 
@@ -17,8 +15,8 @@ const inputPriceElement = document.getElementById('item-price');
 
     // 販売手数料と販売利益を表示する要素に結果をセット
     if (addTaxPriceElement && profitElement) {
-      addTaxPriceElement.textContent = fee.toFixed(2);
-      profitElement.textContent = profit.toFixed(2);
+      addTaxPriceElement.textContent = Math.floor(fee); // 小数点以下切り捨て
+      profitElement.textContent = Math.floor(profit); // 小数点以下切り捨て
     }
   });
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,7 @@ class Item < ApplicationRecord
   validates :delivery_date_id, presence: { message: "can't be blank" }, numericality: { other_than: 1, message: 'must be other than ---' }, exclusion: { in: [nil], message: "can't be blank" }
   validates :shipping_area_id, presence: { message: "can't be blank" }, numericality: { other_than: 1, message: 'must be other than ---' }, exclusion: { in: [nil], message: "can't be blank" }
   validates :item_image, presence: { message: "can't be blank" }
+  validates :category_id, presence: { message: "can't be blank" }, numericality: { other_than: 1, message: 'must be other than ---' }  # この行を追加
   # def sold_out?
   #   self.sold  
   # end
